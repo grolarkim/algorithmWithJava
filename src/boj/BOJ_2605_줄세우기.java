26cba5a06a7e4cdfb4d4945583d7aed1c301f0e5
MyeongjunKim <audwnswmf@naver.com>
2022-07-21 15:47:45 +0900

    Solve boj2605

---
+++ src/boj/BOJ_2605_줄세우기.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ_2605_줄세우기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int[] arr = new int[a];
+		for(int i=0;i<a;i++) {
+			arr[i] = sc.nextInt();
+		}
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i=0;i<a;i++) {
+			list.add(list.size()-arr[i], i+1);
+		}
+		for(int i = 0;i<a;i++) {
+			System.out.print(list.get(i)+" ");
+		}
+		
+	}
+}
