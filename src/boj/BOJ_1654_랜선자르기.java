4a9695b0516bbef938879e43a266615763f9c432
MyeongjunKim <audwnswmf@naver.com>
2022-07-28 17:37:58 +0900

    Solve boj1654

---
+++ src/boj/BOJ_1654_랜선자르기.java
@@ -0,0 +1,45 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1654_랜선자르기 {
+	public static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(in.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[K];
+		long low = 0;
+		long high = 0;
+
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(in.readLine());
+			high = Math.max(high, arr[i]);
+		}
+		
+		high++;
+		
+		while (low < high) {
+			long mid = (low + high) / 2;
+			long sum = 0;
+			for (int a : arr) {
+				sum += (a / mid);
+			}
+			if (sum >= N) {
+				low = mid+1;
+			} else {
+				high = mid;
+			}
+		}
+
+		System.out.println(low-1);
+
+	}
+
+}
