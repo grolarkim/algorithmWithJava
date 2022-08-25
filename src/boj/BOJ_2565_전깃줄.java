ccdb21903daec1c95e18b11950f7746de4e54b8e
김명준 <audwnswmf@naver.com>
2022-08-25 13:45:55 +0900

    Solve boj2565

---
+++ src/boj/BOJ_2565_전깃줄.java
@@ -0,0 +1,45 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BOJ_2565_전깃줄 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int[][] arr = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+
+		int[] dp = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i][1] > arr[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		}
+		int max = 0;
+		for (int i : dp) {
+			max = Math.max(max, i);
+		}
+
+		System.out.println(N - max);
+
+	}
+}
