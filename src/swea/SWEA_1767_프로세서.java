cca1827cc0c3d07715b80f3dbff582cff4577ca1
김명준 <audwnswmf@naver.com>
2022-09-23 15:46:05 +0900

    Solve boj17435

--- src/swea/SWEA_1767_프로세서.java
+++ src/swea/SWEA_1767_프로세서.java
@@ -22,8 +22,9 @@ public class SWEA_1767_프로세서 {
 		T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
 			N = sc.nextInt();
-			table = new int[N][N];
-			processors = new ArrayList<int[]>();
+			table = new int[N][N]; // 프로세서가 올라간 기판 정보
+			processors = new ArrayList<int[]>(); // 프로세서의 위치 리스트
+			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					table[i][j] = sc.nextInt();
@@ -32,12 +33,13 @@ public class SWEA_1767_프로세서 {
 					}
 				}
 			}
-			minArr = new int[processors.size() + 1];
+			minArr = new int[processors.size() + 1]; // 연결길이 최소값을 저장할 배열
 			Arrays.fill(minArr, Integer.MAX_VALUE);
-			maxConnected = 0;
 
+			// 0번째 프로세서, 연결길이 0, 연결된 프로세서개수 0 부터 탐색시작
 			dfs(0, 0, 0);
-
+			
+			//minArr에서 결과값 빼옴
 			getResult();
 
 			System.out.println("#" + tc + " " + result);
@@ -45,7 +47,10 @@ public class SWEA_1767_프로세서 {
 		}
 		sc.close();
 	}
-
+	
+	/**
+	 * minArr에 담겨있는 최소연결길이 정보 중 연결개수가 가장 많은 값을 찾아서 반환 
+	 */
 	private static void getResult() {
 		for (int i = minArr.length - 1; i >= 0; i--) {
 			if (minArr[i] != Integer.MAX_VALUE) {
@@ -55,34 +60,53 @@ public class SWEA_1767_프로세서 {
 		}
 
 	}
-
+	
+	/**
+	 * 탐색에 사용한다. 최악의 경우 5^12
+	 * @param depth 탐색하는 깊이 => depth번째 프로세서를 의미한다
+	 * @param sum 연결길이의 총합
+	 * @param connected 연결된 프로세서의 개수
+	 */
 	private static void dfs(int depth, int sum, int connected) {
+		// 기저조건 minArr에 연결 개수와 길이 값을 저장
 		if (depth == processors.size()) {
 			minArr[connected] = Math.min(minArr[connected], sum);
 			return;
 		}
-
+		
+		// depth번째 프로세서
 		int[] processor = processors.get(depth);
 		int x = processor[0];
 		int y = processor[1];
-
+		
+		//프로세서가 외부와 접한 경우
 		if (x == 0 || x == N - 1 || y == 0 || y == N - 1) {
 			dfs(depth + 1, sum, connected + 1);
 			return;
 		}
 
+		// 4방향으로 확인, 연결
 		for (int dir = 0; dir < 4; dir++) {
 			if (isPossible(x, y, dir)) {
 				int length = connect(x, y, dir, 2);
 				dfs(depth + 1, sum + length, connected + 1);
 				connect(x, y, dir, 0);
-			} else {
-				dfs(depth + 1, sum, connected);
-			}
+			} 			
 		}
+		
+		// 연결이 안 될때
+		dfs(depth + 1, sum, connected);
 
 	}
-
+	
+	/**
+	 * 프로세서를 연결 시키기위해 맵에 칠을 함
+	 * @param x 프로세서의 row값
+	 * @param y 프로세서의 column값
+	 * @param dir 연결하는 방향
+	 * @param target 칠하는 숫자 => 연결시킬때는 2를 쓰고 복구할때는 0을 씀
+	 * @return
+	 */
 	private static int connect(int x, int y, int dir, int target) {
 		int cnt = 0;
 		int i = 0;
@@ -100,6 +124,13 @@ public class SWEA_1767_프로세서 {
 		return cnt;
 	}
 
+	/**
+	 * 프로세서에서 이 방향으로 연결이 가능한지 여부 반환
+	 * @param x 프로세서의 row값
+	 * @param y 프로세서의 column값
+	 * @param dir 연결하는 방향
+	 * @return
+	 */
 	private static boolean isPossible(int x, int y, int dir) {
 		int i = 1;
 		while (i <= N) {
