c7754014dc789c1f156aa8f70922a58f8292d17a
김명준 <audwnswmf@naver.com>
2023-04-13 15:32:28 +0900

    solve boj 10775

--- src/boj/BOJ_10775_공항.java
+++ src/boj/BOJ_10775_공항.java
@@ -6,29 +6,42 @@ import java.io.InputStreamReader;
 
 public class BOJ_10775_공항 {
     static int G, P;
-    static boolean[] gates;
     static int[] nextGates;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         G = Integer.parseInt(br.readLine());
         P = Integer.parseInt(br.readLine());
-        gates = new boolean[G + 1];
         nextGates = new int[G + 1];
         for (int i = 0; i <= G; i++) {
             nextGates[i] = i;
         }
+        int cnt = 0;
         for (int i = 0; i < P; i++) {
-
+            int a = Integer.parseInt(br.readLine());
+            int b = getNextGate(a);
+            if (b == 0) {
+                break;
+            }
+            union(b, b - 1);
+            cnt++;
         }
+
+        System.out.println(cnt);
 
 
     }
 
+    private static void union(int x, int y) {
+        int a = getNextGate(x);
+        int b = getNextGate(y);
+        nextGates[a] = b;
+    }
+
     private static int getNextGate(int x) {
-        if (nextGates[x] == x) {
-            return x;
+        if (x == nextGates[x]) {
+            return nextGates[x];
         }
-        return nextGates[x] = getNextGate(x);
+        return nextGates[x] = getNextGate(nextGates[x]);
     }
 }
